6b77f29446fe661cec7f404bd78c4c76e52bf1f1
senattm <senaatim95@hotmail.com>
2025-10-10 13:40:01 +0300

    feat(image-slider): add image slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-import './App.css'
-import Accordion from './components/accordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
+import { useState } from "react";
+import "./App.css";
+import Accordion from "./components/accordion";
+import RandomColor from "./components/random-color";
+import StarRating from "./components/star-rating";
+import ImageSlider from "./components/image-slider";
 
 function App() {
-
-  return (
-   <StarRating/>
-  )
+ return (
+    <ImageSlider
+      url={"https://picsum.photos/v2/list"}
+      limit={"8"}
+    />
+  );
 }
 
-export default App
+/*  return (
+    <ImageSlider
+      url={"https://picsum.photos/v2/list"}
+      limit={"8"}
+    />
+  );
+
+  image slider açmak için
+  */
+export default App;
