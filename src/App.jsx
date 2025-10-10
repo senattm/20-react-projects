4475be9012ec9e3ecb8b46b6016ae755c81e9c1b
senattm <senaatim95@hotmail.com>
2025-10-10 10:19:41 +0300

    feat(star-rating): add interactive star rating with hover & click selection

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import './App.css'
 import Accordion from './components/accordion'
 import RandomColor from './components/random-color'
+import StarRating from './components/star-rating'
 
 function App() {
 
   return (
-   <RandomColor/>
+   <StarRating/>
   )
 }
 
